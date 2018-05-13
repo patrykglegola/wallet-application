cf4e1b3b527d21da8536e53c92ee0cbf21a596d0
Patryk Glegoła <patryk.glegola@gmail.com>
2018-05-13 18:04:24 +0200

    Added basic authentication entry point config.
    Source: http://www.devglan.com/spring-security/spring-boot-security-rest-basic-authentication

---
+++ src/main/java/pl/glegdev/wallet/config/AuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package pl.glegdev.wallet.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Solution from http://www.devglan.com/spring-security/spring-boot-security-rest-basic-authentication
+ * used to make basic authentication for testing REST API with Postman.
+ */
+
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+            throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" +getRealmName());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        setRealmName("DeveloperStack");
+        super.afterPropertiesSet();
+    }
+
+}
