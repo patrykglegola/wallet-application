2822d79f7fb94d1a1c735eed4d0d6533aff43c2f
Patryk Glegoła <patryk.glegola@gmail.com>
2018-05-14 22:19:06 +0200

    Deleted authentication entry point and updated Spring Security config.

--- src/main/java/pl/glegdev/wallet/config/SpringSecurityConfig.java
+++ src/main/java/pl/glegdev/wallet/config/SpringSecurityConfig.java
@@ -1,40 +1,25 @@
 package pl.glegdev.wallet.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-/**
- * Solution from http://www.devglan.com/spring-security/spring-boot-security-rest-basic-authentication
- * used to make basic authentication for testing REST API with Postman.
- */
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
-    private AuthenticationEntryPoint authEntryPoint;
-
-    @Autowired
-    public SpringSecurityConfig(AuthenticationEntryPoint authEntryPoint) {
-        this.authEntryPoint = authEntryPoint;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .anyRequest().authenticated()
-                .and().httpBasic()
-                .authenticationEntryPoint(authEntryPoint);
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("$2a$04$x.hP4mCIALu0hh.spHC1o.7IeGIitk4HlLKFZtxtjmbivPtV.49Du").roles("USER");
+        http.csrf().disable();
+        http.httpBasic();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users").anonymous()
+                .antMatchers(HttpMethod.GET, "/users/all").anonymous()
+                .and()
+            .authorizeRequests()
+                .anyRequest().permitAll();
     }
 
 }
