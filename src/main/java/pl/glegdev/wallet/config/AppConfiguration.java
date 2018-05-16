9c3b0025b22f308f0c7044fa650e2787b262e69e
Patryk Glegoła <patryk.glegola@gmail.com>
2018-05-16 11:26:34 +0200

    Added CORS requests configuration.

--- src/main/java/pl/glegdev/wallet/config/AppConfiguration.java
+++ src/main/java/pl/glegdev/wallet/config/AppConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class AppConfiguration {
@@ -13,4 +16,15 @@ public class AppConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+
+    // CORS requests configuration:
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
